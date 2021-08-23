cc067e23b9f5e860d09d7da2055d6b85b6c12895
Jefferson Cantores <jeffersoncantores13@gmail.com>
2021-08-23 11:52:05 +0800

    Added a header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     //BEM class naming convention
     <div className="app">
-      <h1>Hi~</h1>
+
+      <Header />
+      {/* TinderCards */}
+      {/* SwipeButtons */}
     </div>
   );
 }
